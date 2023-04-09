a65c6e788d5353ba8e65b7d034eff00e31f9b3d9
dynamiteroll <adelprado548@gmail.com>
2023-04-09 16:15:45 -0700

    authentication sortof working

--- pokeapifrontend/src/App.js
+++ pokeapifrontend/src/App.js
@@ -7,31 +7,35 @@ import Login from "./components/Login";
 import HomePage from "./components/HomePage";
 function App() {
 
-  const [pageNo, setPageNo] = useState(1);
-  const [typeSelectedArray, setSelectedTypeArray] = useState([]);
+
 
     
   return (
     <div className="App">
       <nav className="navbar">
-        <h1>DateDex.com</h1>
+        <h1>Dated Dex.com</h1>
       </nav>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/login" element={<Login/>}/>
+      {/* <Router>
+        <Routes> */}
+          {/* <Route path="/" element={<HomePage/>} />
+          <Route path="/login" element={<Login/>}/> */}
           {/* <Route element={<PrivateRoutes/>}>
-            <Route path="/pokedex" element={<Search setSelectedTypeArray={setSelectedTypeArray} typeSelectedArray={typeSelectedArray} setPageNo={setPageNo}/>}/>
-            <Route path="/pokedex/:pageNo" element={<Search setSelectedTypeArray={setSelectedTypeArray} typeSelectedArray={typeSelectedArray} setPageNo={setPageNo}/>}/>
+                <Route path="/pokedex" element={
+                <>
+                  <Search setSelectedTypeArray={setSelectedTypeArray} typeSelectedArray={typeSelectedArray} setPageNo={setPageNo}/>
+                  <FilterPokemons typeSelectedArray={typeSelectedArray} setPageNo={setPageNo} pageNo={pageNo}/>
+                </>
+              }/>
           </Route> */}
-          <Route path="/pokedex" element={
+          {/* <Route path="/pokedex" element={
           <>
             <Search setSelectedTypeArray={setSelectedTypeArray} typeSelectedArray={typeSelectedArray} setPageNo={setPageNo}/>
             <FilterPokemons typeSelectedArray={typeSelectedArray} setPageNo={setPageNo} pageNo={pageNo}/>
           </>
-        }/>
-        </Routes>
-      </Router>
+          }/> */}
+        {/* </Routes>
+      </Router> */}
+      <Login></Login>
 
       
     </div>
--- pokeapifrontend/src/components/Login.js
+++ pokeapifrontend/src/components/Login.js
@@ -1,57 +1,115 @@
 
-// import React, { useState } from 'react'
-// import axios from 'axios'
-// import Dashboard from './Dashboard';
+import React, { useState } from 'react'
+import axios from 'axios'
+import Dashboard from './Dashboard';
+import {Routes, Route} from "react-router-dom";
 
-// function Login() {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [user, setUser] = useState({});
-//   const [accessToken, setAccessToken] = useState('');
-//   const [refreshToken, setRefreshToken] = useState('');
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     try {
-//       const res = await axios.post("http://localhost:5000/login", { username, password });
-//       setUser(res.data);
-//       setAccessToken(res.headers['auth-token-access']);
-//       setRefreshToken(res.headers['auth-token-refresh']);
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
+import Search from "./Search";
+import FilterPokemons from "./FilterPokemons";
 
-//   return (
-//     <div>
-//       {user?.username ? (
-//         <>
-//           <h1>Welcome {user.username}</h1>
-//           <Dashboard accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} />
-//         </>
-//       ) : (
-//         <form onSubmit={handleSubmit}>
-//           <span> Admin Login </span>
-//           <br />
-//           <input
-//             type="text"
-//             placeholder="username"
-//             onChange={(e) => setUsername(e.target.value)}
-//           />
-//           <br />
-//           <input
-//             type="password"
-//             placeholder="password"
-//             onChange={(e) => setPassword(e.target.value)}
-//           />
-//           <br />
-//           <button type="submit">
-//             Login
-//           </button>
-//         </form>
-//       )}
-//     </div>
-//   )
-// }
+function Login() {
 
-// export default Login
+    const [pageNo, setPageNo] = useState(1);
+    const [typeSelectedArray, setSelectedTypeArray] = useState([]);
+
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState({});
+  const [accessToken, setAccessToken] = useState('');
+  const [refreshToken, setRefreshToken] = useState('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/login", { username, password });
+      setUser(res.data);
+      setAccessToken(res.headers['auth-token-access']);
+      setRefreshToken(res.headers['auth-token-refresh']);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    // <div>
+    //   {user?.username ? (
+    //     <>
+    //       <h1>Welcome {user.username}</h1>
+    //       <Dashboard accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} />
+    //     </>
+    //   ) : (
+        // <form onSubmit={handleSubmit}>
+        //   <span> Admin Login </span>
+        //   <br />
+        //   <input
+        //     type="text"
+        //     placeholder="username"
+        //     onChange={(e) => setUsername(e.target.value)}
+        //   />
+        //   <br />
+        //   <input
+        //     type="password"
+        //     placeholder="password"
+        //     onChange={(e) => setPassword(e.target.value)}
+        //   />
+        //   <br />
+        //   <button type="submit">
+        //     Login
+        //   </button>
+        // </form>
+    //   )}
+    // </div>
+
+    <div>
+        {
+        (accessToken && user?.role === "admin") && 
+            // <Routes>
+            //     <Route path="/dashboard" element={<Dashboard accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} />}/>
+            // </Routes>
+            <Dashboard accessToken={accessToken} setAccessToken={setAccessToken} refreshToken={refreshToken} />
+        }
+        {
+            (accessToken && user?.role === "user") && 
+            
+            // <Routes>
+            //     <Route path="/pokedex" element={
+            //         <>
+            //           <Search setSelectedTypeArray={setSelectedTypeArray} typeSelectedArray={typeSelectedArray} setPageNo={setPageNo}/>
+            //           <FilterPokemons typeSelectedArray={typeSelectedArray} setPageNo={setPageNo} pageNo={pageNo}/>
+            //         </>
+            //     }/>
+            // </Routes>
+            <>
+                <Search setSelectedTypeArray={setSelectedTypeArray} typeSelectedArray={typeSelectedArray} setPageNo={setPageNo}/>
+                <FilterPokemons typeSelectedArray={typeSelectedArray} setPageNo={setPageNo} pageNo={pageNo}/>
+            </>
+        }
+        {
+        (!accessToken) && 
+
+        <form onSubmit={handleSubmit}>
+            <span> Admin Login </span>
+            <br />
+            <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+            />
+            <br />
+            <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <br />
+            <button type="submit">
+            Login
+            </button>
+        </form>
+        }
+    </div>
+  )
+}
+
+export default Login
 
